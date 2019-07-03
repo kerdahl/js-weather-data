d221ddad79c29ed9c92253a654797ed1ae96bad1
Kyle Erdahl <kyle.m.erdahl@gmail.com>
2019-07-02 21:36:20 -0500

    added optional environment port variable, renamed server data file to server.js

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const Datastore = require('nedb');
 const app = express();
 const fetch = require('node-fetch');
 
-app.listen(3000, () => console.log('listening at 3000'));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`listening at port ${port}`));
 
 app.use(express.static('public'));
 app.use(express.json({
