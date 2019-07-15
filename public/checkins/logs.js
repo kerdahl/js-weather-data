930b239c06a70a421d54606a7b122e298667f508
Kyle Erdahl <kyle.m.erdahl@gmail.com>
2019-07-15 10:44:19 -0500

    Fixed line spacings. Should use 2 spaces per indent, not 4.

--- public/checkins/logs.js
+++ public/checkins/logs.js
@@ -2,7 +2,7 @@ const mymap = L.map('mapid').setView([0, 0], 1);
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 const tiles = L.tileLayer(tileUrl, {
-    attribution
+  attribution
 });
 
 tiles.addTo(mymap);
@@ -10,26 +10,26 @@ tiles.addTo(mymap);
 getData();
 
 async function getData() {
-    const response = await fetch('/api');
-    const data = await response.json();
+  const response = await fetch('/api');
+  const data = await response.json();
 
-    console.log(data);
+  console.log(data);
 
-    for (item of data) {
+  for (item of data) {
 
-        const marker = L.marker([item.lat, item.long]).addTo(mymap);
+    const marker = L.marker([item.lat, item.long]).addTo(mymap);
 
-        const cityString = item.place.city;
+    const cityString = item.place.city;
 
-        const timestamp = (new Date(item.timestamp)).toLocaleString();
+    const timestamp = (new Date(item.timestamp)).toLocaleString();
 
-        let aqString = "There is no air quality reading for this location.";
-        if (item.airQuality.results.length > 0) {
-            aqString = `The concentration of particulate matter (${item.airQuality.results[0].measurements[0].parameter}) was ${item.airQuality.results[0].measurements[0].value} ${item.airQuality.results[0].measurements[0].unit} as of ${item.airQuality.results[0].measurements[0].lastUpdated}`;
-        }
-
-        let txt = `The weather in ${cityString} (${item.lat}°, ${item.long}°) was ${item.weather.summary} with a temperature of ${item.weather.temperature}°F at ${timestamp}. ${aqString}`;
-
-        marker.bindPopup(txt);
+    let aqString = "There is no air quality reading for this location.";
+    if (item.airQuality.results.length > 0) {
+      aqString = `The concentration of particulate matter (${item.airQuality.results[0].measurements[0].parameter}) was ${item.airQuality.results[0].measurements[0].value} ${item.airQuality.results[0].measurements[0].unit} as of ${item.airQuality.results[0].measurements[0].lastUpdated}`;
     }
+
+    let txt = `The weather in ${cityString} (${item.lat}°, ${item.long}°) was ${item.weather.summary} with a temperature of ${item.weather.temperature}°F at ${timestamp}. ${aqString}`;
+
+    marker.bindPopup(txt);
+  }
 }
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.listen(port, () => console.log(`listening at port ${port}`));
 
 app.use(express.static('public'));
 app.use(express.json({
-    limit: '1mb'
+  limit: '1mb'
 }));
 
 const database = new Datastore('database.db');
@@ -20,75 +20,75 @@ const weather_api_key = process.env.WEATHER_API_KEY;
 const google_api_key = process.env.GOOGLE_API_KEY;
 
 const googleMapsClient = require('@google/maps').createClient({
-    key: google_api_key,
-    Promise: Promise
+  key: google_api_key,
+  Promise: Promise
 });
 
 app.get('/api', (request, response) => {
-    database.find({}, (err, data) => {
-        if (err) {
-            response.end;
-            return;
-        }
-        response.json(data);
-    });
+  database.find({}, (err, data) => {
+    if (err) {
+      response.end;
+      return;
+    }
+    response.json(data);
+  });
 
 });
 
 app.post('/api', (request, response) => {
-    const data = request.body;
-    const timestamp = Date.now();
-    data.timestamp = timestamp;
-    database.insert(data);
-    response.json(data);
+  const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
+  response.json(data);
 })
 
 app.get('/weather/:lat/:long', async (request, response) => {
-    console.log(request.params);
-    const api_url = `https://api.darksky.net/forecast/${weather_api_key}/${request.params.lat},${request.params.long}`;
-    const weatherResponse = await fetch(api_url);
-    const weatherData = await weatherResponse.json();
+  console.log(request.params);
+  const api_url = `https://api.darksky.net/forecast/${weather_api_key}/${request.params.lat},${request.params.long}`;
+  const weatherResponse = await fetch(api_url);
+  const weatherData = await weatherResponse.json();
 
-    const air_url = `https://api.openaq.org/v1/latest?coordinates=${request.params.lat},${request.params.long}`;
-    const airResponse = await fetch(air_url);
-    const airData = await airResponse.json();
+  const air_url = `https://api.openaq.org/v1/latest?coordinates=${request.params.lat},${request.params.long}`;
+  const airResponse = await fetch(air_url);
+  const airData = await airResponse.json();
 
-    let locationData = {
-        city: "Unknown City",
-        state: "Unknown State"
-    };
+  let locationData = {
+    city: "Unknown City",
+    state: "Unknown State"
+  };
 
-    googleMapsClient.reverseGeocode({
-            latlng: [request.params.lat, request.params.long],
-        })
-        .asPromise()
-        .then((geoResponse) => {
-            if (geoResponse.status == 200) {
-                const geoData = geoResponse.json;
-                if (geoData.results[1]) {
-                    var city = false,
-                        state = false;
-                    for (var i = 0; i < geoData.results.length; i++) {
-                        if ((!city || !state) && geoData.results[i].types[0] === "locality") {
-                            city = geoData.results[i].address_components[0].short_name,
-                                state = geoData.results[i].address_components[2].short_name;
-                            locationData = {
-                                city: city,
-                                state: state
-                            };
-                        }
-                    }
-                }
+  googleMapsClient.reverseGeocode({
+      latlng: [request.params.lat, request.params.long],
+    })
+    .asPromise()
+    .then((geoResponse) => {
+      if (geoResponse.status == 200) {
+        const geoData = geoResponse.json;
+        if (geoData.results[1]) {
+          var city = false,
+            state = false;
+          for (var i = 0; i < geoData.results.length; i++) {
+            if ((!city || !state) && geoData.results[i].types[0] === "locality") {
+              city = geoData.results[i].address_components[0].short_name,
+                state = geoData.results[i].address_components[2].short_name;
+              locationData = {
+                city: city,
+                state: state
+              };
             }
+          }
+        }
+      }
 
-            const data = {
-                weather: weatherData,
-                air_quality: airData,
-                location: locationData
-            };
+      const data = {
+        weather: weatherData,
+        air_quality: airData,
+        location: locationData
+      };
 
-            response.json(data);
-        });
+      response.json(data);
+    });
 });
 
 exports = module.exports = app;
