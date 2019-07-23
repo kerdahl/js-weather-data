f1d7a95838e03046a97e5b4717450512a5b145ca
Kyle Erdahl <kyle.m.erdahl@gmail.com>
2019-07-23 18:28:37 -0500

    Changed data source to Firebase Updated node server to use Firebase over NeDB Updated HTML pages to reference Firebase instead of NeDB Added Firebase tests to server.js tests Deleted database.db Updated .env-sample to contain necessary firebase template Removed NeDB from package.json Added Firebase to package.json

--- server.js
+++ server.js
@@ -1,74 +1,111 @@
 require('dotenv').config();
 const express = require('express');
-const Datastore = require('nedb');
 const app = express();
 const fetch = require('node-fetch');
+const firebase = require('firebase');
+require('firebase/firestore');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`listening at port ${port}`));
-app.use(express.static('public'));
-app.use(express.json({ limit: '1mb' }));
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: '',
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
 
-const database = new Datastore('database.db');
-database.loadDatabase();
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
 
 const weather_api_key = process.env.WEATHER_API_KEY;
-const google_api_key = process.env.GOOGLE_API_KEY;
 
+const google_api_key = process.env.GOOGLE_API_KEY;
 const googleMapsClient = require('@google/maps').createClient({
   key: google_api_key,
   Promise: Promise
 });
 
-app.get('/api', (request, response) => {
-  database.find({}).sort({ timestamp: 1 }).exec((err, data) => {
-    if (err) {
-      response.end;
-      return;
-    }
-    response.json(data);
-  });
+app.listen(port, () => console.log(`listening at port ${port}`));
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
 
+app.get('/api', (request, response) => {
+  let data = [];
+  let weatherCollection = db.collection('weather').orderBy('timestamp', 'asc');
+  weatherCollection
+    .get()
+    .then(query => {
+      query.forEach(weatherEntry => {
+        data.push(weatherEntry.data());
+      });
+
+      response.json(data);
+    })
+    .catch(error => {
+      console.error(`Error getting data: ${error}`);
+      response.end;
+    });
 });
 
 app.post('/api', (request, response) => {
   const data = request.body;
   const timestamp = Date.now();
   data.timestamp = timestamp;
-  database.insert(data);
+
+  let weatherCollection = db.collection('weather');
+  weatherCollection
+    .add(data)
+    .then(docRef => {
+      console.log(`Document written, ID ${docRef.id}`);
+    })
+    .catch(error => {
+      console.error(`Error adding document: ${error}`);
+    });
+
   response.json(data);
-})
+});
 
 app.get('/weather/:lat/:long', async (request, response) => {
   console.log(request.params);
-  const api_url = `https://api.darksky.net/forecast/${weather_api_key}/${request.params.lat},${request.params.long}`;
+  const api_url = `https://api.darksky.net/forecast/${weather_api_key}/${
+    request.params.lat
+  },${request.params.long}`;
   const weatherResponse = await fetch(api_url);
   const weatherData = await weatherResponse.json();
 
-  const air_url = `https://api.openaq.org/v1/latest?coordinates=${request.params.lat},${request.params.long}`;
+  const air_url = `https://api.openaq.org/v1/latest?coordinates=${
+    request.params.lat
+  },${request.params.long}`;
   const airResponse = await fetch(air_url);
   const airData = await airResponse.json();
 
   let locationData = {
-    city: "Unknown City",
-    state: "Unknown State"
+    city: 'Unknown City',
+    state: 'Unknown State'
   };
 
-  googleMapsClient.reverseGeocode({
-      latlng: [request.params.lat, request.params.long],
+  googleMapsClient
+    .reverseGeocode({
+      latlng: [request.params.lat, request.params.long]
     })
     .asPromise()
-    .then((geoResponse) => {
+    .then(geoResponse => {
       if (geoResponse.status == 200) {
         const geoData = geoResponse.json;
         if (geoData.results[1]) {
           var city = false,
             state = false;
           for (var i = 0; i < geoData.results.length; i++) {
-            if ((!city || !state) && geoData.results[i].types[0] === "locality") {
-              city = geoData.results[i].address_components[0].short_name,
-                state = geoData.results[i].address_components[2].short_name;
+            if (
+              (!city || !state) &&
+              geoData.results[i].types[0] === 'locality'
+            ) {
+              (city = geoData.results[i].address_components[0].short_name),
+                (state = geoData.results[i].address_components[2].short_name);
               locationData = {
                 city: city,
                 state: state
--- test/server.js
+++ test/server.js
@@ -1,31 +1,117 @@
 require('dotenv').config();
-let expect = require('chai').expect;
-let request = require('request');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = require('chai').expect;
 
 const port = process.env.PORT || 3000;
 
-// eslint-disable-next-line no-unused-vars
-let app = require('../server');
+chai.use(chaiHttp);
+
+require('../server');
 
 describe('Server Status', () => {
   after(() => {
-    console.log("Tests complete.");
+    console.log('Tests complete.');
     process.exit(0);
   });
 
-  it('Main Page', (done) => {
-    // eslint-disable-next-line no-unused-vars
-    request(`http://localhost:${port}`, (err, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      done();
-    });
+  it('Main Page', done => {
+    chai
+      .request(`http://localhost:${port}`)
+      .get('/')
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        done();
+      });
   });
 
-  it('Check-Ins Page', (done) => {
-    // eslint-disable-next-line no-unused-vars
-    request(`http://localhost:${port}/checkins`, (err, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      done();
-    });
+  it('Check-Ins Page', done => {
+    chai
+      .request(`http://localhost:${port}`)
+      .get('/checkins')
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  it('firebase data should be an array', done => {
+    chai
+      .request(`http://localhost:${port}`)
+      .get('/api')
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('Array').that.is.not.empty;
+        done();
+      });
+  });
+
+  it('firebase data should have a timestamp', done => {
+    chai
+      .request(`http://localhost:${port}`)
+      .get('/api')
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res.body[0]).to.have.property('timestamp');
+        done();
+      });
+  });
+
+  it('firebase data should have air quality data', done => {
+    chai
+      .request(`http://localhost:${port}`)
+      .get('/api')
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res.body[0]).to.have.property('airQuality');
+        done();
+      });
+  });
+
+  it('firebase data should have a latitude', done => {
+    chai
+      .request(`http://localhost:${port}`)
+      .get('/api')
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res.body[0]).to.have.property('lat');
+        done();
+      });
+  });
+
+  it('firebase data should have a longitude', done => {
+    chai
+      .request(`http://localhost:${port}`)
+      .get('/api')
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res.body[0]).to.have.property('long');
+        done();
+      });
+  });
+
+  it('firebase data should have a place', done => {
+    chai
+      .request(`http://localhost:${port}`)
+      .get('/api')
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res.body[0]).to.have.property('place');
+        done();
+      });
+  });
+
+  it('firebase data should have weather information', done => {
+    chai
+      .request(`http://localhost:${port}`)
+      .get('/api')
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res.body[0]).to.have.property('weather');
+        done();
+      });
   });
 });
